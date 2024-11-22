779df02d73e4acb0acc8337f959f9b7444bbfaed
Vivian James <vivianjames2928@gmail.com>
2024-11-22 10:57:20 +0100

    visa application website

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,3 +19,23 @@ window.addEventListener("scroll", () => {
     navbars.classList.remove("scrolled");
   }
 });
+
+
+function sendEmail() {
+  const email = document.getElementById('emailInput').value;
+  if (email) {
+    alert('Subcribed to our newsletter');
+  } else {
+    alert('Please enter a valid email address')
+  }
+}
+
+function sendEmail() {
+  const emailInput = document.getElementById("emailInput").value;
+
+  if (emailInput) {
+    alert("You've successfully subscribed");
+  } else {
+    alert("Please enter something!");
+  }
+}
